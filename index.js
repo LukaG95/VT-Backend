501525f3ef683ae252beb40a5564beb1681301e8
RyuRL <rleague04@gmail.com>
2020-09-04 17:53:14 +0200

    content-type err fix at3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('./startup/prod')(app)
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/public', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
