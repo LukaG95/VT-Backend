864ee7501e7cf5f8cd654753158bcfb294c2944f
RyuRL <rleague04@gmail.com>
2021-02-09 18:56:23 +0100

    f

--- Controllers/messagesController.js
+++ Controllers/messagesController.js
@@ -1,13 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const { Messages, validateMessage, validateMessageQuery } = require("../Models/MessagesModel");
-const { User } = require("../Models/userModel");
-const { readableActiveAt } = require("../misc/time");
+const { Messages, validateMessage, validateMessageQuery } = require('../Models/messagesModel')
+const { User } = require('../Models/userModel')
+const {readableActiveAt} = require('../misc/time')
 
-<<<<<<< HEAD
-exports.getMessages = async (req, res, next) => {
-    const user = await User.findById(req.user.id).select("-__v");
-=======
 exports.getDialogues = async (req, res, next) => {
 	const user = await User.findById(req.user.id).select('-__v');
 	// let { page } = req.query;
@@ -61,30 +57,15 @@ exports.getDialogues = async (req, res, next) => {
 	
 	return res.status(200).json({ info: 'success', dialogues });
 }
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
 
-    const { recipientId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(recipientId))
-        return res.status(400).json({ info: "recipientId", message: "Invalid recipientId" });
+exports.getMessagesWithUser = async (req, res, next) => {
+	const user = await User.findById(req.user.id).select('-__v');
+	
+	const { recipientId } = req.params;
+	let { page } = req.query;
+	if(!page) page = 1;
+	page--;
 
-<<<<<<< HEAD
-    participants = { "participants.0": user._id, "participants.1": user._id };
-    if (recipientId < user._id) participants["participants.0"] = recipientId;
-    else participants["participants.1"] = recipientId;
-
-    console.log(participants);
-    const messages = await Messages.find(participants)
-        .populate(["participants.0", "participants.1"])
-        .sort("-createdAt");
-    console.log(messages);
-    if (messages.length < 1)
-        return res
-            .status(200)
-            .json({ info: "no messages", message: "user has no messages with the recipient", messages: [] });
-
-    return res.status(200).json({ info: "success", messages: messages });
-};
-=======
 	if (!mongoose.Types.ObjectId.isValid(recipientId)) return res.status(400).json({info: "recipientId", message: "Invalid recipientId"});
 	
 	participants = { 'participants.0': user._id, 'participants.1': user._id };
@@ -96,32 +77,15 @@ exports.getDialogues = async (req, res, next) => {
 	
 	return res.status(200).json({ info: 'success', messages: messages});
 }
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
 
 exports.sendMessage = async (req, res, next) => {
-    const user = await User.findById(req.user.id).select("-__v");
-    const { recipientId, message } = req.body;
+	const user = await User.findById(req.user.id).select('-__v');
 
-    const { error } = await validateMessage(req.body, user, req);
-    if (error) return res.status(400).json({ info: "invalid credentials", message: error.details[0].message });
+	const { recipientId, message } = req.body;
+	
+	const { error } = await validateMessage(req.body, user, req);
+	if (error) return res.status(400).json({info: "invalid credentials", message: error.details[0].message});
 
-<<<<<<< HEAD
-    participants = { 0: user._id, 1: user._id };
-    if (recipientId < user._id) participants[0] = recipientId;
-    else participants[1] = recipientId;
-
-    const messageDetails = {
-        participants: participants,
-        message: message,
-        sender: user._id == participants[0] ? 0 : 1,
-        createdAt: Date.now(),
-        editedAt: Date.now()
-    };
-
-    await new Messages(messageDetails).save();
-    return res.status(200).json({ info: "success", message: "message was send" });
-};
-=======
 	const recipientDB = await User.findById(recipientId).select('-__v');
 	if (!recipientDB) return res.status(400).json({info: "invalid receiver", message: 'recepient could not be found'})
 	
@@ -195,12 +159,11 @@ exports.sendMessage = async (req, res, next) => {
 // 	}
 // 	return res.status(200).json({info: "success", message: "user in no longer blocked"});
 // }
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
 
 exports.editMessage = async (req, res, next) => {
-    return res.status(501).json({ info: "not implemented", message: "editing messages is not implemented" });
-};
+	return res.status(501).json({info: "not implemented", message: "editing messages is not implemented"})
+}
 
 exports.deleteMessage = async (req, res, next) => {
-    return res.status(501).json({ info: "not implemented", message: "deleting messages is not implemented" });
-};
+	return res.status(501).json({info: "not implemented", message: "deleting messages is not implemented"})
+}
--- Routes/messagesRoutes.js
+++ Routes/messagesRoutes.js
@@ -1,32 +1,21 @@
-const express = require("express");
+const express = require('express')
 
-const authController = require("../Controllers/authController");
-const messagesController = require("../Controllers/messagesController");
+const limiter = require('../misc/generalRateLimiter')(5, 3)
+const authController = require('../Controllers/authController')
+const messagesController = require('../Controllers/messagesController')
 
-const router = express.Router();
+const router = express.Router()
 
-<<<<<<< HEAD
-router.get("/:recipientId", authController.protect, messagesController.getMessages);
-=======
 router.get('/', authController.protect, messagesController.getDialogues)
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
 
-router.post("/message", authController.protect, messagesController.sendMessage);
+router.get('/:recipientId', authController.protect, messagesController.getMessagesWithUser)
 
-<<<<<<< HEAD
-router.put("/message", authController.protect, messagesController.editMessage);
-=======
 router.post('/message', authController.protect, limiter, messagesController.sendMessage)
 // router.post('/blockUser', authController.protect, limiter, messagesController.blockUser)
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
 
-router.delete("/message", authController.protect, messagesController.deleteMessage);
+router.put('/message', authController.protect, messagesController.editMessage)
 
-<<<<<<< HEAD
-module.exports = router;
-=======
 // router.delete('/blockUser', authController.protect, limiter, messagesController.unblockUser)
 router.delete('/message', authController.protect, messagesController.deleteMessage)
 
 module.exports = router
->>>>>>> 811b92f8b8953cfa2eb1754ff7fc723306cfe1b1
